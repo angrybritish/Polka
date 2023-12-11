7176c6d7bddb5f8acc1e34dea584b4d12b32f3f7
Nastya <147645596+Nastyaaaaaaaaaaaa@users.noreply.github.com>
2023-12-11 20:47:30 +0300

    Add files via upload

--- Polka/src/main/java/com/example/Polka/PolkaApplication.java
+++ Polka/src/main/java/com/example/Polka/PolkaApplication.java
@@ -2,13 +2,10 @@ package com.example.Polka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"Controller"})
 public class PolkaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PolkaApplication.class, args);
 	}
-
 }
